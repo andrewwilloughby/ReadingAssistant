a50a2de04a82eca0d34d774dfd9ed2990b06c603
Andrew Willoughby <andrew.willoughby5@gmail.com>
2016-11-28 00:03:58 +0000

    DataParser class
    Completed implementation of DataParser class.

--- Campus_Assistant-master/app/src/main/java/com/example/andrewwilloughby/campus_assistant/DownloadUrl.java
+++ Campus_Assistant-master/app/src/main/java/com/example/andrewwilloughby/campus_assistant/DownloadUrl.java
@@ -2,7 +2,6 @@ package com.example.andrewwilloughby.campus_assistant;
 
 import android.util.Log;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
