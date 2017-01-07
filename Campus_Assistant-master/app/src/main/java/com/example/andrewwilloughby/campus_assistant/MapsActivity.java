ef5864ee59c40adbce3f922e8f3bf1947d19e1da
Andrew Willoughby <andrew.willoughby5@gmail.com>
2017-01-07 12:42:54 +0000

    Completed Maps Search Bar
    Completed code for Maps search bar functionality.

--- Campus_Assistant-master/app/src/main/java/com/example/andrewwilloughby/campus_assistant/MapsActivity.java
+++ Campus_Assistant-master/app/src/main/java/com/example/andrewwilloughby/campus_assistant/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.andrewwilloughby.campus_assistant;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
@@ -9,6 +12,9 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -26,6 +32,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
@@ -37,6 +46,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     double latitude;
     double longitude;
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private Button search_Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +64,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } else {
             Log.d("onCreate", "Google Play Services available");
         }
+
+        search_Btn = (Button) findViewById(R.id.searchBtn);
+        search_Btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                onMapSearch(view);
+            }
+        });
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -74,6 +91,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return true;
     }
 
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        //Clear existing markers.
+        mMap.clear();
+
+        if (location != null) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title(address.getFeatureName()));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
 
     /**
      * Manipulates the map once available.
