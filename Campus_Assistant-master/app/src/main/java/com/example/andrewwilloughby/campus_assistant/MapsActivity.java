59301ff69b20f3fddc150fcc485d1dac1a986b4e
Andrew Willoughby <andrew.willoughby5@gmail.com>
2017-01-08 19:08:37 +0000

    MapsActivity update: Keyboard closes on search
    When search is executed, Android virtual keyboard is closed.

--- Campus_Assistant-master/app/src/main/java/com/example/andrewwilloughby/campus_assistant/MapsActivity.java
+++ Campus_Assistant-master/app/src/main/java/com/example/andrewwilloughby/campus_assistant/MapsActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -101,6 +102,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //Clear existing markers.
         mMap.clear();
+
+        //Close virtual keyboard.
+
+        if (view != null){
+            InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
 
         if (location != null) {
             Geocoder geocoder = new Geocoder(this);
@@ -269,6 +277,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-
+        return false;
     }
 }
